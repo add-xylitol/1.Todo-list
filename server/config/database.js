f5e2ecc9f105f44d661d674598d57efd24f9a98b
xylitol <18616528897@163.com>
2025-07-28 17:18:11 +0800

    Switch to MySQL in production for Netlify

--- server/config/database.js
+++ server/config/database.js
@@ -5,8 +5,12 @@ const { Sequelize } = require('sequelize');
 const path = require('path');
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: '/tmp/database.sqlite',
+  dialect: process.env.NODE_ENV === 'production' ? 'mysql' : 'sqlite',
+  storage: process.env.NODE_ENV !== 'production' ? '/tmp/database.sqlite' : undefined,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   logging: (msg) => logger.info(msg)
 });
 
