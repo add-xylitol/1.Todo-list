9210a7618a27db0e8657926a9807691927562419
xylitol <18616528897@163.com>
2025-07-29 19:26:19 +0800

    Add logging for environment variables in database config

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,12 @@ const logger = require('../utils/logger');
 const { Sequelize } = require('sequelize');
 const path = require('path');
 
-const sequelize = new Sequelize({
+console.log('Environment variables:', {
+    NODE_ENV: process.env.NODE_ENV,
+    CONTEXT: process.env.CONTEXT,
+    AWS_LAMBDA_FUNCTION_NAME: process.env.AWS_LAMBDA_FUNCTION_NAME
+  });
+  const sequelize = new Sequelize({
   dialect: process.env.AWS_LAMBDA_FUNCTION_NAME ? 'mysql' : 'sqlite',
   storage: process.env.AWS_LAMBDA_FUNCTION_NAME ? undefined : '/tmp/database.sqlite',
   host: process.env.DB_HOST,
